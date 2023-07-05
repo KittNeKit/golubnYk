fcee948b0e21d8736b3017d179499966a8980b81
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-05 21:39:53 +0300

    Fixed permissions

--- social/permissions.py
+++ social/permissions.py
@@ -1,13 +1,13 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+class IsAdminOrIfAuthenticated(BasePermission):
     def has_permission(self, request, view):
         return bool(
             (
-                request.method in SAFE_METHODS
-                and request.user
+                request.user
                 and request.user.is_authenticated
             )
             or (request.user and request.user.is_staff)
         )
+
